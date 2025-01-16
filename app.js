b6ddcf7d4af84d8782f9225419a194f160ae6e71
MohitDhull05 <mohitdhull191@gmail.com>
2025-01-16 12:35:40 +0530

    responsive

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 $('.slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    centerMode: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true,
   variableWidth: true,
-    dots: true,
-    arrows: false,
-    infinity: true
+  dots: true,
+  arrows: false,
+  infinity: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        sliderToShow: 3,
+        slidesToScroll: 1,
+        infinity: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        sliderToShow: 3,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        sliderToShow: 3,
+        slidesToScroll: 1,
+      }
+    }
+
+  ]
 });
